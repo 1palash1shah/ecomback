619db9b5386ba185ae78e0bb3e5e75a329722de1
Palash-shah <palashshah110@gmail.com>
2023-06-20 16:46:00 +0530

    Try Image

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import AWS from "aws-sdk";
+import multer from "multer";
+import multerS3 from 'multer-s3';
 dotenv.config();
 
+const s3 = new AWS.S3();
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -55,17 +60,18 @@ const ProductSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    ProductImgs: [{
-      Img1 : {
-      type: String,
+    ProductImgs: [
+      {
+        Img1: {
+          type: String,
+        },
+        Img2: {
+          type: String,
+        },
+        Img3: {
+          type: String,
+        },
       },
-      Img2 : {
-      type: String,
-      },
-      Img3 : {
-      type: String,
-      },
-    }
     ],
     ProductCategory: {
       type: String,
@@ -95,12 +101,14 @@ const ProductSchema = mongoose.Schema(
     ProductReviewComment: {
       type: String,
     },
-  created_at:{ type: Date },
-  updated_at:{ type: Date, default: Date.now }
-}, { versionKey: false },{strict: false});
+    created_at: { type: Date },
+    updated_at: { type: Date, default: Date.now },
+  },
+  { versionKey: false },
+  { strict: false }
+);
 
 const Products = new mongoose.model("Products", ProductSchema);
-
 
 app.post("/AddProduct", (req, res) => {
   const {
@@ -136,13 +144,45 @@ app.post("/AddProduct", (req, res) => {
     ProductReviewComment,
   });
   UploadProduct.save()
-  .then((res)=>{
-    res.status(200).send({message:"Item Added",Data:res});
-  })
-  .catch((err) => {
-    console.log("unable to save to db", err);
-    res.status(402).send("unable to save to database");
-  });
+    .then((res) => {
+      res.status(200).send({ message: "Item Added", Data: res });
+    })
+    .catch((err) => {
+      console.log("unable to save to db", err);
+      res.status(402).send("unable to save to database");
+    });
+});
+
+const s3Storage = multerS3({
+  s3: s3, // s3 instance
+  bucket: process.env.CYCLIC_BUCKET_NAME, // change it as per your project requirement
+  acl: "public-read", 
+  metadata: (req, file, cb) => {
+    cb(null, { fieldname: file.fieldname });
+  },
+  key: (req, file, cb) => {
+    const fileName =
+      Date.now() + "_" + file.fieldname + "_" + file.originalname;
+    cb(null, fileName);
+  },
+});
+
+const uploadImage = multer({
+  storage: s3Storage,
+  limits: {
+    fileSize: 1024 * 1024 * 2, // 2mb file size
+  },
+});
+
+app.post("/uploadImage", uploadImage.single("image"), async (req, res) => {
+  let data = {};
+  if (req.file) {
+    data.image = req.file.location;
+    console.log(req.file.location);
+  }
+  res.send(data);
+  console.log(data);
+
 });
 
 app.get("/", (req, res) => {
